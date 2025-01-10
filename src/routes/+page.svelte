39f188d2842dc4edd100799dad050a354467bd98
Panda <84963742+0Pandab@users.noreply.github.com>
2025-01-10 11:00:11 +0100

    Anfang

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="box-content flex">
+	<div>
+		<img src="/images/logo.svg" alt="Logo Rock Paper Scissors" />
+	</div>
+	<div>Servus</div>
+</div>
