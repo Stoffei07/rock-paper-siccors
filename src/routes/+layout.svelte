88fc8cd0ddef3ec43f8979a95c17bccd72d10178
Panda <84963742+0Pandab@users.noreply.github.com>
2025-01-17 11:09:49 +0100

    Design_First_Finished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,6 @@
 	let { children } = $props();
 </script>
 
-<div class="gradient flex min-h-screen flex-col items-center p-8">
+<div class="gradient flex min-h-screen flex-col items-center justify-between p-8">
 	{@render children()}
 </div>
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,48 @@
 <script>
+	let step = $state(1);
 </script>
 
 <div
-	class="mx-auto flex w-full max-w-screen-lg justify-between rounded-md border-4 border-[#606e85] p-6"
+	class="mx-auto flex w-full max-w-screen-lg justify-between rounded-2xl border-4 border-[#606e85] p-6"
 >
 	<div>
 		<img src="/images/logo.svg" alt="Logo Rock Paper Scissors" />
 	</div>
-	<div>
-		<div class="w-28 rounded-md bg-white p-4">
-			<div class="barlow-600 flex justify-center tracking-widest text-[#2a46c0]">Score</div>
-			<div class="barlow-700 flex justify-center text-4xl text-[#3b4363]">12</div>
-		</div>
+
+	<div class="w-28 rounded-md bg-white p-4">
+		<div class="barlow-600 flex justify-center tracking-widest text-[#2a46c0]">Score</div>
+		<div class="barlow-700 flex justify-center text-4xl text-[#3b4363]">12</div>
 	</div>
 </div>
+{#if step === 1}
+	<div
+		class="bg-img grid aspect-square w-full max-w-[400px] grid-cols-4 grid-rows-2 gap-[10%] bg-center"
+	>
+		<button
+			class="border-gradient-paper col-span-2 flex aspect-square w-full items-center justify-center rounded-full border-[15px] border-yellow-500
+			bg-white"><img src="/images/icon-paper.svg" alt="" /></button
+		>
+		<button
+			class="col-span-2 flex aspect-square w-full items-center justify-center rounded-full border-[15px] border-blue-500 bg-white"
+			><img src="/images/icon-scissors.svg" alt="" /></button
+		>
+		<button
+			class="col-span-2 col-start-2 flex aspect-square w-full items-center justify-center rounded-full border-[15px] border-red-500 bg-white"
+			><img src="/images/icon-rock.svg" alt="" /></button
+		>
+	</div>
+{/if}
+
+<button
+	class="barlow-600 flex w-[125px] items-center justify-center rounded-lg border-2 border-[#FFF] p-2 tracking-widest text-white lg:self-end"
+>
+	RULES
+</button>
+
+<style>
+	.bg-img {
+		background-image: url('/images/bg-triangle.svg');
+		background-size: 70%;
+		background-repeat: no-repeat;
+	}
+</style>
